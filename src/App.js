8da6ea8ae2ea6194c9f2fd2a1cf8c48ab439f329
ewertonSoares <ewerton.fa4@gmail.com>
2021-05-14 22:10:05 -0300

    Escondendo Header

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Login from './components/Login/Login';
 import Header from './components/Header/Header';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+
+import './App.css';
 
 
 const text = `Lorem Ipsum is simply dummy text of the printing and 
@@ -18,59 +21,78 @@ essentially unchanged. It was popularised in the 1960s with the release of Letra
 sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
 software like Aldus PageMaker including versions of Lorem Ipsum.`;
 
+const news = [
+  {
+    id: 1,
+    image: `https://s2.glbimg.com/QqEDyJyWVPTI9tU-5izKpJls6UE=/620x520/smart/
+    e.glbimg.com/og/ed/f/original/2020/11/30/baby-yoda.jpg`,
+    title: "Titulo 1",
+    text: text
+  },
+  {
+    id: 2,
+    image: `https://pm1.narvii.com/6340/818cf5d1a6a2f6693413168c50ab0b64177f49c2_hq.jpg`,
+    title: "Titulo 2",
+    text: text
+  },
+  {
+    id: 3,
+    image: `https://miro.medium.com/max/485/1*NPFbsnC4K9b5xPbEz6LoJw.jpeg`,
+    title: "Titulo 3",
+    text: text
+  }
+]
 
-function App() {
+export default class App extends React.Component {
+  constructor() {
+    super();
 
-  const [news, setNews] = useState(
-    [
-      {
-        id: 1,
-        image: `https://s2.glbimg.com/QqEDyJyWVPTI9tU-5izKpJls6UE=/620x520/smart/
-        e.glbimg.com/og/ed/f/original/2020/11/30/baby-yoda.jpg`,
-        title: "Titulo 1",
-        text: text
-      },
-      {
-        id: 2,
-        image: `https://pm1.narvii.com/6340/818cf5d1a6a2f6693413168c50ab0b64177f49c2_hq.jpg`,
-        title: "Titulo 2",
-        text: text
-      },
-      {
-        id: 3,
-        image: `https://miro.medium.com/max/485/1*NPFbsnC4K9b5xPbEz6LoJw.jpeg`,
-        title: "Titulo 3",
-        text: text
-      }
-    ]
-  )
+    this.state = {
+      classHide: 'show'
+    }
+  }
 
-  return (
-    <BrowserRouter>
+  changeState() {
+    var st = '';
+    if (this.state.classHide == 'show') {
+      st = 'hide';
+    } else {
+      st = 'show'
+    }
 
-    <div className="App">
-      <Header />
+    this.setState({
+      classHide: st
+    })
+  }
 
-      <Switch>
-        <Redirect from='/' to='/news' exact />
-        <Route path="/news" exact>
-        <NewsCard news={news}></NewsCard>
-        </Route>
+  render() {
+    return (
+      <BrowserRouter>
 
-        <Route path="/news/:id" exact>
-          <NewDetails />
-        </Route>
+        <div className="App">
+          <Header hideHeader={this.state.classHide} changeState={this.changeState.bind(this)} />
 
-        <Route path="/login" exact>
-          <Login />
-        </Route>
+          <Switch>
+            <Redirect from='/' to='/news' exact />
+            <Route path="/news" exact>
+              <NewsCard news={news}></NewsCard>
+            </Route>
 
-        <Route render={() => <div> Pagina não encontrada </div>} />
-        </Switch>
-    </div>
+            <Route path="/news/:id" exact>
+              <NewDetails />
+            </Route>
 
-    </BrowserRouter>
-  );
+            <Route path="/login" exact>
+              <Login />
+            </Route>
+
+            <Route render={() => <div> Pagina não encontrada </div>} />
+          </Switch>
+        </div>
+
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
+// export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,34 @@
+import React from 'react';
 import { Form, Button, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom'
 
 import './Header.css'
 
-function Header() {
-  return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">InfoNews</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="/news">Início</Nav.Link>
-          <Nav.Link href="#features">Assine</Nav.Link>
-        </Nav>
-        <Form inline>
-          <NavLink to="/login" exact><Button variant="outline-info">Entrar</Button></NavLink>
-        </Form>
-      </Navbar>
-    </>
-  );
+
+export default class Header extends React.Component {
+
+  render() {
+    return (
+      <div className={this.props.hideHeader}>
+
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/news">DailyNews</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/news">Início</Nav.Link>
+            <Nav.Link href="#features">Assine</Nav.Link>
+
+          </Nav>
+          <Form inline>
+            <NavLink to="/login" exact>
+              <Button variant="outline-info" onClick={this.props.changeState}>
+                Entrar
+              </Button>
+            </NavLink>
+          </Form>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
-export default Header;
+// export default Header;
